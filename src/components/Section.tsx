bdaa4d2531490e89165b4b4fdf04eba47bef33f4
PrettyCoffee <jonathan-holz@outlook.com>
2022-01-08 19:28:54 +0100

    feat(Section): Add left margin for content

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,14 +5,15 @@ import styled from "@emotion/styled/macro"
 
 const Title = styled.h2`
   ${({ theme: { color, space } }) => css`
-    margin: 0;
+    margin: 0 0 0 -${space.medium};
     border-bottom: ${space.smallest} solid ${color.bg.shade};
   `}
 `
 
 const Wrapper = styled.div`
   ${({ theme: { space } }) => css`
-    padding-bottom: ${space.largest};
+    margin-bottom: ${space.largest};
+    margin-left: ${space.medium};
     :last-of-type {
       padding-bottom: 0;
     }
