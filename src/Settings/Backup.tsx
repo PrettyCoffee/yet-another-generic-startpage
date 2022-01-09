937da388c50d3fc78059060e3dbf75db2751557a
PrettyCoffee <jonathan-holz@outlook.com>
2022-01-09 21:46:02 +0100

    feat(Backup): Add backup selection

--- src/Settings/Backup.tsx
+++ src/Settings/Backup.tsx
@@ -5,6 +5,8 @@ import { storageBackup, useStoragePrefix } from "@startpage/local-storage"
 import { Download } from "react-feather"
 
 import { Button, Section, FileInput } from "../components"
+import { Checkbox } from "../components/Checkbox"
+import { CenterLayout } from "./Design/fragments/CenterLayout"
 
 const Layout = styled.div`
   display: flex;
@@ -13,9 +15,17 @@ const Layout = styled.div`
 `
 
 export const Backup = () => {
+  const [useBookmarks, setUseBookmarks] = useState(true)
+  const [useColors, setUseColors] = useState(true)
+  const [useSettings, setUseSettings] = useState(true)
   const [importValid, setImportValid] = useState<boolean>()
   const prefix = useStoragePrefix()
-  const keys = Object.keys(localStorage).filter(key => key.startsWith(prefix))
+
+  const keys = []
+  if (useBookmarks) keys.push(prefix + "bookmarks")
+  if (useColors) keys.push(prefix + "theme")
+  if (useSettings) keys.push(prefix + "settings")
+
   const Backup = storageBackup(keys)
 
   const onImport = (file: File) => {
@@ -24,6 +34,19 @@ export const Backup = () => {
   }
   return (
     <Section title="Backup">
+      <CenterLayout>
+        <Checkbox
+          label="Bookmarks"
+          checked={useBookmarks}
+          onChange={setUseBookmarks}
+        />
+        <Checkbox label="Colors" checked={useColors} onChange={setUseColors} />
+        <Checkbox
+          label="Miscellaneous"
+          checked={useSettings}
+          onChange={setUseSettings}
+        />
+      </CenterLayout>
       <br />
       <Layout>
         <FileInput
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,76 @@
+import { css } from "@emotion/react"
+import styled from "@emotion/styled/macro"
+import { Check } from "react-feather"
+
+const Label = styled.label`
+  ${({ theme: { space } }) => css`
+    cursor: pointer;
+    height: calc(${space.medium} * 2);
+    margin: ${space.medium};
+
+    display: inline-flex;
+    align-items: center;
+    gap: ${space.medium};
+  `}
+`
+
+const Box = styled.span`
+  ${({ theme: { space, color } }) => css`
+    position: relative;
+    height: ${space.medium};
+    width: ${space.medium};
+
+    display: inline-flex;
+    align-items: center;
+
+    border: ${color.fg.base} solid ${space.smallest};
+    outline: none;
+
+    :focus-visible {
+      border-color: ${color.primary.base};
+    }
+
+    label:hover > & {
+      background-color: ${color.bg.shade};
+    }
+
+    > svg {
+      display: none;
+      position: absolute;
+      width: 24px;
+      bottom: -4px;
+      left: -2px;
+    }
+    &[aria-checked="true"] > svg {
+        display: block;
+      }
+    }
+  `}
+`
+
+const LabelText = styled.span``
+
+type CheckboxProps = {
+  checked: boolean
+  onChange?: (checked: boolean) => void
+  label: string
+}
+
+export const Checkbox = ({ checked, onChange, label }: CheckboxProps) => {
+  const handleChange = () => onChange?.(!checked)
+  const handleKeyPress = (key: string) => key === " " && handleChange()
+
+  return (
+    <Label onClick={handleChange}>
+      <Box
+        role="checkbox"
+        aria-checked={checked}
+        tabIndex={0}
+        onKeyPress={event => handleKeyPress(event.key)}
+      >
+        <Check />
+      </Box>
+      <LabelText>{label}</LabelText>
+    </Label>
+  )
+}
