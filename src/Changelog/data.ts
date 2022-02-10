624eb1b144b5eba09accea17ac689f6c1178c17b
PrettyCoffee <jonathan-holz@outlook.com>
2022-02-10 21:03:23 +0100

    chore: Release v1.1.1

--- src/Changelog/data.ts
+++ src/Changelog/data.ts
@@ -7,6 +7,10 @@ export type Changelog = {
 
 export const changelog: Changelog = sortChangelogDesc([
   {
+    version: "v1.1.1",
+    changes: ["fix changelog link styling bug"],
+  },
+  {
     version: "v1.1.0",
     changes: [
       "added google fonts toggle (folks tend to dislike google)",
